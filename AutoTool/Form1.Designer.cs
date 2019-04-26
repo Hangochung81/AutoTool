fdf15e119b8d2a81b177002fe9e9cf9b6026bd09
Hung Ha <hangochung81@gmail.com>
2019-04-26 09:58:00 +0700

    First CM

---
+++ AutoTool/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AutoTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtExcelPath = new System.Windows.Forms.TextBox();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.ExecuteBtn = new System.Windows.Forms.Button();
+            this.OpenButton2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Excel Report";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ResultFolder";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(84, 40);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(100, 20);
+            this.txtPath.TabIndex = 2;
+            // 
+            // txtExcelPath
+            // 
+            this.txtExcelPath.Location = new System.Drawing.Point(84, 71);
+            this.txtExcelPath.Name = "txtExcelPath";
+            this.txtExcelPath.ReadOnly = true;
+            this.txtExcelPath.Size = new System.Drawing.Size(100, 20);
+            this.txtExcelPath.TabIndex = 3;
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(197, 38);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton.TabIndex = 4;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // ExecuteBtn
+            // 
+            this.ExecuteBtn.Location = new System.Drawing.Point(184, 207);
+            this.ExecuteBtn.Name = "ExecuteBtn";
+            this.ExecuteBtn.Size = new System.Drawing.Size(75, 23);
+            this.ExecuteBtn.TabIndex = 5;
+            this.ExecuteBtn.Text = "Execute";
+            this.ExecuteBtn.UseVisualStyleBackColor = true;
+            // 
+            // OpenButton2
+            // 
+            this.OpenButton2.Location = new System.Drawing.Point(197, 69);
+            this.OpenButton2.Name = "OpenButton2";
+            this.OpenButton2.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton2.TabIndex = 6;
+            this.OpenButton2.Text = "Open";
+            this.OpenButton2.UseVisualStyleBackColor = true;
+            this.OpenButton2.Click += new System.EventHandler(this.OpenButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.OpenButton2);
+            this.Controls.Add(this.ExecuteBtn);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.txtExcelPath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtExcelPath;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button ExecuteBtn;
+        private System.Windows.Forms.Button OpenButton2;
+    }
+}
+
---
+++ AutoTool/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+        private void Open_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = fbd.SelectedPath;
+            }
+            //fbd.ShowDialog();
+            
+
+        }
+
+        private void OpenButton2_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtExcelPath.Text = fbd.SelectedPath;
+            }
+        }
+
+
+
+    }
+}
